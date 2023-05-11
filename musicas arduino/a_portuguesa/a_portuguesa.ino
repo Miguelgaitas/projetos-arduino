f9d1c9501e4f40a24f0d91e9c886ce2c4dcc2ae9
Miguelgaitas <89109806+Miguelgaitas@users.noreply.github.com>
2023-05-11 15:14:30 +0100

    Add files via upload

---
+++ musicas arduino/a_portuguesa/a_portuguesa.ino
@@ -0,0 +1,267 @@
+#define DO3 132
+#define RE3 148 
+#define MI3 166
+#define FA3 176
+#define FAS3 186
+#define SOL3 197
+#define LA3 222
+#define SI3 249
+#define DO4 264
+#define RE4 294 
+#define MI4 330
+#define MIB4 314
+#define FA4 349
+#define FAS4 370
+#define SOL4 392
+#define SOLS4 419
+#define LA4 440
+#define SI4 494
+#define SIB4 467
+#define DO5 528
+#define RE5 592
+#define MI5 665
+
+int buzzer = 11;
+
+void setup() 
+{
+    pinMode(buzzer,OUTPUT);    
+}
+
+void loop() 
+{
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+  	tone(buzzer,FA4);    
+    delay(300);
+  	tone(buzzer,MI4);    
+    delay(300);
+  	tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,SI3);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,FAS4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,SI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(1600);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SI4);    
+    delay(300);
+    tone(buzzer,RE5);    
+    delay(300);
+    tone(buzzer,SI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(1600);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,DO5);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SIB4);    
+    delay(300);
+    tone(buzzer,SOLS4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(1300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,MIB4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,MIB4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,DO5);    
+    delay(300);
+    tone(buzzer,SI4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,DO3);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,RE4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,DO5);    
+    delay(300);
+    tone(buzzer,SI4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,DO5);    
+    delay(300);
+    tone(buzzer,MI5);    
+    delay(300);
+    tone(buzzer,RE5);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,DO4);    
+    delay(300);
+    tone(buzzer,MI4);    
+    delay(300);
+    tone(buzzer,FA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,LA4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,SOL4);    
+    delay(300);
+    tone(buzzer,DO5);    
+    delay(300);
+    tone(buzzer,DO5);    
+    delay(300);
+}
